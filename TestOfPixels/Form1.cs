fbee8a837232d8a6424810bf61fbac09a9e82017
Yotic <55879406+MrYotic@users.noreply.github.com>
2021-10-25 23:15:08 +0300

    Update up to 35 strings

--- TestOfPixels/Form1.cs
+++ TestOfPixels/Form1.cs
@@ -1,4 +1,4 @@
-﻿namespace a{
+namespace a{
     public partial class aa : System.Windows.Forms.Form{
         public aa(){
             a111();
@@ -8,8 +8,7 @@
         internal void a1(int x, int y, bool aaaa){ 
             System.Drawing.Bitmap a1a = (System.Drawing.Bitmap)a11[x, y].Image;
             System.Drawing.Color aa1 = aaaa == true ? System.Drawing.Color.DarkGreen : System.Drawing.Color.LightGreen;
-            for (int i = 1; i < 24 * 24; i++)
-                  a1a.SetPixel(i / 24, i % 24, aa1);
+            for (int i = 1; i < 24 * 24; i++) a1a.SetPixel(i / 24, i % 24, aa1);
             a11[x, y].Image = a1a;}
         int[] aaa1 = new int[2] { 15, 15};
         internal void aaa(System.Random r1, System.Random r2){
@@ -23,13 +22,11 @@
             a11[aaa1[0], aaa1[1]].Image = aa1a;}
         internal System.Windows.Forms.Keys aa11 = System.Windows.Forms.Keys.W;
         protected override bool ProcessCmdKey(ref System.Windows.Forms.Message a1a1, System.Windows.Forms.Keys a1aa){
-            for (int o = 0; o < aa1.Count; o++)
-                if (((a1aa == System.Windows.Forms.Keys.W ? 0 : (a1aa == System.Windows.Forms.Keys.A ? 1 : (a1aa == System.Windows.Forms.Keys.D ? 2 : (a1aa == System.Windows.Forms.Keys.S ? 3 : 100)))) == 0 ? (aa1[o][1] == aa1[0][1] - 1 && aa1[o][0] == aa1[0][0]) : ((a1aa == System.Windows.Forms.Keys.W ? 0 : (a1aa == System.Windows.Forms.Keys.A ? 1 : (a1aa == System.Windows.Forms.Keys.D ? 2 : (a1aa == System.Windows.Forms.Keys.S ? 3 : 100)))) == 1 ? (aa1[o][1] == aa1[0][1] && aa1[o][0] == aa1[0][0] - 1) : ((a1aa == System.Windows.Forms.Keys.W ? 0 : (a1aa == System.Windows.Forms.Keys.A ? 1 : (a1aa == System.Windows.Forms.Keys.D ? 2 : (a1aa == System.Windows.Forms.Keys.S ? 3 : 100)))) == 2 ? (aa1[o][1] == aa1[0][1] && aa1[o][0] == aa1[0][0] + 1) : ((aa1[o][1] == aa1[0][1] + 1 && aa1[o][0] == aa1[0][0]))))) && (a1aa == System.Windows.Forms.Keys.W ? 0 : (a1aa == System.Windows.Forms.Keys.A ? 1 : (a1aa == System.Windows.Forms.Keys.D ? 2 : (a1aa == System.Windows.Forms.Keys.S ? 3 : 100)))) < 5) return true;
+            for (int o = 0; o < aa1.Count; o++) if (((a1aa == System.Windows.Forms.Keys.W ? 0 : (a1aa == System.Windows.Forms.Keys.A ? 1 : (a1aa == System.Windows.Forms.Keys.D ? 2 : (a1aa == System.Windows.Forms.Keys.S ? 3 : 100)))) == 0 ? (aa1[o][1] == aa1[0][1] - 1 && aa1[o][0] == aa1[0][0]) : ((a1aa == System.Windows.Forms.Keys.W ? 0 : (a1aa == System.Windows.Forms.Keys.A ? 1 : (a1aa == System.Windows.Forms.Keys.D ? 2 : (a1aa == System.Windows.Forms.Keys.S ? 3 : 100)))) == 1 ? (aa1[o][1] == aa1[0][1] && aa1[o][0] == aa1[0][0] - 1) : ((a1aa == System.Windows.Forms.Keys.W ? 0 : (a1aa == System.Windows.Forms.Keys.A ? 1 : (a1aa == System.Windows.Forms.Keys.D ? 2 : (a1aa == System.Windows.Forms.Keys.S ? 3 : 100)))) == 2 ? (aa1[o][1] == aa1[0][1] && aa1[o][0] == aa1[0][0] + 1) : ((aa1[o][1] == aa1[0][1] + 1 && aa1[o][0] == aa1[0][0]))))) && (a1aa == System.Windows.Forms.Keys.W ? 0 : (a1aa == System.Windows.Forms.Keys.A ? 1 : (a1aa == System.Windows.Forms.Keys.D ? 2 : (a1aa == System.Windows.Forms.Keys.S ? 3 : 100)))) < 5) return true;
             aa11 = a1aa;
             return base.ProcessCmdKey(ref a1a1, a1aa);}
         private void a11a(object aaaaa, System.EventArgs e){
-            for (int i = 0; i < aa1.Count; i++)
-                if ((aa11 == System.Windows.Forms.Keys.W ? (aa1[0][0] == aa1[i][0] && aa1[0][1] - 1 == aa1[i][1]) : (aa11 == System.Windows.Forms.Keys.A ? (aa1[0][0] - 1 == aa1[i][0] && aa1[0][1] == aa1[i][1]) : (aa11 == System.Windows.Forms.Keys.D ? (aa1[0][0] + 1 == aa1[i][0] && aa1[0][1] == aa1[i][1]) : (aa11 == System.Windows.Forms.Keys.S ? (aa1[0][0] == aa1[i][0] && aa1[0][1] + 1 == aa1[i][1]) : false))))) System.Diagnostics.Process.GetCurrentProcess().Kill();
+            for (int i = 0; i < aa1.Count; i++) if ((aa11 == System.Windows.Forms.Keys.W ? (aa1[0][0] == aa1[i][0] && aa1[0][1] - 1 == aa1[i][1]) : (aa11 == System.Windows.Forms.Keys.A ? (aa1[0][0] - 1 == aa1[i][0] && aa1[0][1] == aa1[i][1]) : (aa11 == System.Windows.Forms.Keys.D ? (aa1[0][0] + 1 == aa1[i][0] && aa1[0][1] == aa1[i][1]) : (aa11 == System.Windows.Forms.Keys.S ? (aa1[0][0] == aa1[i][0] && aa1[0][1] + 1 == aa1[i][1]) : false))))) System.Diagnostics.Process.GetCurrentProcess().Kill();
             a1(aa1[0][0] + (aa11 == System.Windows.Forms.Keys.A ? - 1 : (aa11 == System.Windows.Forms.Keys.D ? + 1 : + 0)), aa1[0][1] + (aa11 == System.Windows.Forms.Keys.W ? - 1 : (aa11 == System.Windows.Forms.Keys.S ? 1 : 0)), true);
             aa1.Insert(0, new int[] { aa1[0][0] + (aa11 == System.Windows.Forms.Keys.A ? -1 : (aa11 == System.Windows.Forms.Keys.D ? +1 : +0)), aa1[0][1] + (aa11 == System.Windows.Forms.Keys.W ? -1 : (aa11 == System.Windows.Forms.Keys.S ? 1 : 0)) });
             if (aaa1[0] == aa1[0][0] && aaa1[1] == aa1[0][1]) aaa(new System.Random(), new System.Random());
